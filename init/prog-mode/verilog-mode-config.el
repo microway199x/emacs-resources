04cd9e8eb77febae3326a1d0d45b44bac6419948
microway199x <microway199@163.com>
2023-08-01 20:51:20 +0800

    update for Verilog-mode align instance module wires

--- init/prog-mode/verilog-mode-config.el
+++ init/prog-mode/verilog-mode-config.el
@@ -1,4 +1,4 @@
-;;* 1. common config 
+;;* {{ 1. common config 
 ;;(setq verilog-active-low-regexp t)  ;;comment for sometimes case verilog-auto-tieoff/reset fail
   (setq verilog-align-declaration-comments t)
   (setq verilog-align-ifelse t)
@@ -18,8 +18,9 @@
 ;;  indent-to变量最大值，设置自动定义变量的宽度,比如 input、output、wire等
 ;; can use load module to use new version plugin
 ;;(load xxx/xx/verilog-mode.el)  ;;example
+;;* }} 1. common config 
 
-;;* 2. outline mode header config
+;;* {{ 2. outline mode header config
 
 (defun v-verilog-outline-hook ()
   ; outline uses this regexp to find headers. 
@@ -44,8 +45,9 @@
 )
 ;;; only when verolog-mode buffer execute
 (add-hook 'verilog-mode-hook 'v-verilog-outline-hook)
+;;* }} 2. outline mode header config
 
-;;======= Code folding =======
+;;* {{ 3. ======= Code folding =======
 ;(add-hook 'python-mode-hook 'my-python-outline-hook)
 ;; this gets called by outline to deteremine the level. Just use the length of the whitespace
 ;(defun py-outline-level ()
@@ -70,5 +72,102 @@
 ;  ; make paren matches visible
 ;  (show-paren-mode 1)
 ;)
+;;* }} 3. ======= Code folding =======
+
+
+;;* {{ 4. personal scripts
+
+;;* {{{ 4.1 verilog instant align
+(defun v-verilog-align-inst ()
+  "verilog align instance connect wires..."
+  (interactive)
+  (setq region-start-p (region-beginning))
+  (setq region-end-p (region-end))
+  (setq region-strings (buffer-substring region-start-p region-end-p))
+  (setq region-line-list (split-string region-strings "\n"))
+  (setq new-region-line-list '())
+  (setq max-inst-len 10)
+  (setq max-con-len 10)
+
+  ;;get instant signal width
+  (dolist (line-string region-line-list)
+    ;;;string match for connect wire: empty float or others
+    (cond ((string-match "\\.[ ]*\\([^ ]*\\)[ ]+([ ]*\\([^ ].*[^ ]\\|[^ ]\\|\\)[ ]*)[ ]*,[ ]*\\(.*\\)" line-string)
+           (progn
+             ( if ( < max-inst-len (length (match-string 1 line-string)))
+                 (setq max-inst-len (length (match-string 1 line-string))))
+             ( if ( < max-con-len (length (match-string 2 line-string)))
+                 (setq max-con-len (length (match-string 2 line-string))))
+             ))
+          ((string-match "\\.[ ]*\\([^ ]*\\)[ ]+([ ]*\\([^ ].*[^ ]\\|[^ ]\\|\\)[ ]*)[ ]*)[ ]*;[ ]*\\(.*\\)" line-string)
+           (progn
+             ( if ( < max-inst-len (length (match-string 1 line-string)))
+                 (setq max-inst-len (length (match-string 1 line-string))))
+             ( if ( < max-con-len (length (match-string 2 line-string)))
+                 (setq max-con-len (length (match-string 2 line-string))))
+             ))
+          ((string-match "\\.[ ]*\\([^ ]*\\)[ ]+([ ]*\\([^ ].*[^ ]\\|[^ ]\\|\\)[ ]*)[ ]*\\(.*\\)" line-string)
+           (progn
+             ( if ( < max-inst-len (length (match-string 1 line-string)))
+                 (setq max-inst-len (length (match-string 1 line-string))))
+             ( if ( < max-con-len (length (match-string 2 line-string)))
+                 (setq max-con-len (length (match-string 2 line-string))))
+             ))
+     );;;end cond
+    ) ;;;end dolist
+
+  ;;;get output string width and format string
+  (setq line-output-format (concat "    .%-"
+                              (format "%d" (* 10 (+ 1 (/ max-inst-len 10))))
+                              "s (%-"
+                              (format "%d" (* 10 (+ 1 (/ max-con-len 10))))
+                              "s )%-2s %-s"))
+  ;;;format output string 
+  (dolist (line-string region-line-list new-region-line-list)
+    (cond ((string-match "\\.[ ]*\\([^ ]*\\)[ ]+([ ]*\\([^ ].*[^ ]\\|[^ ]\\|\\)[ ]*)[ ]*,[ ]*\\(.*\\)" line-string)
+           (progn
+             (setq new-line-string (format line-output-format
+                                           (match-string 1 line-string)
+                                           (match-string 2 line-string)
+                                           ","
+                                           (match-string 3 line-string)))))
+          ((string-match "\\.[ ]*\\([^ ]*\\)[ ]+([ ]*\\([^ ].*[^ ]\\|[^ ]\\|\\)[ ]*)[ ]*)[ ]*;[ ]*\\(.*\\)" line-string)
+           (progn
+             (setq new-line-string (format line-output-format
+                                           (match-string 1 line-string)
+                                           (match-string 2 line-string)
+                                           ");"
+                                           (match-string 3 line-string)))))
+          ((string-match "\\.[ ]*\\([^ ]*\\)[ ]+([ ]*\\([^ ].*[^ ]\\|[^ ]\\|\\)[ ]*)[ ]*\\(.*\\)" line-string)
+           (progn
+             (setq new-line-string (format line-output-format
+                                           (match-string 1 line-string)
+                                           (match-string 2 line-string)
+                                           ""
+                                           (match-string 3 line-string)))))
+          ;;;default: output origin string
+          (t
+               (setq new-line-string line-string))
+     );;;end cond
+    (setq new-region-line-list (append new-region-line-list (list new-line-string)))
+    ) ;;;end dolist
+
+  ;;;output and replace region string
+  (delete-region region-start-p region-end-p)
+  (dolist (line-string new-region-line-list)
+    (progn
+      (message line-string )
+      (insert line-string "\n" )
+      )
+    )
+  ) ;;; end func v-verilog-align-inst
+
+
+;;* }}} 4.1 verilog instant align
+
+
+;;* }} 4. personal scripts
+
+
 
 (provide 'verilog-mode-config)
